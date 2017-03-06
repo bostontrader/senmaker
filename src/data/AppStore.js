65808c4e9c3ef6001a3822dc83dbf87db93ca064
Thomas Radloff <bostontrader@gmail.com>
2017-03-07 06:15:13 +0800

    AppStore should use browser local storage if available.

--- src/data/AppStore.js
+++ src/data/AppStore.js
@@ -6,6 +6,8 @@ import AppDispatcher from './AppDispatcher'
 import {NounPanelLevel} from '../data/nouns/NounConstants'
 import {VerbPanelLevel} from '../data/verbs/VerbConstants'
 
+const localStorageKey = 'AppStore'
+
 class AppStore extends ReduceStore {
 
     constructor() {
@@ -13,42 +15,77 @@ class AppStore extends ReduceStore {
     }
 
     getInitialState() {
-        return {currentAppLevel:AppStore.theLevels[0], minLevel:true, maxLevel:false, quiz:false}
+
+        localStorage.removeItem(localStorageKey)
+        function storageAvailable(type) {
+            try {
+                const storage = window[type], x = '__storage_test__'
+                storage.setItem(x, x)
+                storage.removeItem(x)
+                return true
+            } catch(e) {
+                return false
+            }
+        }
+
+        if (storageAvailable('localStorage')) {
+            AppStore.localStorageAvailable = true
+            const localStorageState = localStorage.getItem(localStorageKey)
+            if(localStorageState)
+                return JSON.parse(localStorageState) // hope the parse works
+        }
+
+        return AppStore.initialState
+
     }
 
     reduce(state, action) {
+        let newState
         switch (action.type) {
-
             case AppActionTypes.LEVEL_PREVIOUS:
-                if(AppStore.currentLevel > 0)
-                    //return AppStore.theLevels[--AppStore.currentLevel]
-                    return {
-                        currentAppLevel:AppStore.theLevels[--AppStore.currentLevel],
-                        minLevel:AppStore.currentLevel <= 0, maxLevel:false,
-                        quiz:AppStore.theQuiz
+                if (AppStore.currentLevel > 0) {
+                    newState = {
+                        currentAppLevel: AppStore.theLevels[--AppStore.currentLevel],
+                        minLevel: AppStore.currentLevel <= 0, maxLevel: false,
+                        quiz: AppStore.theQuiz
                     }
-                else
+                    if(AppStore.localStorageAvailable)
+                        localStorage.setItem(localStorageKey, JSON.stringify(newState))
+
+                    return newState
+                } else
                     return state
 
             case AppActionTypes.LEVEL_NEXT:
-                if(AppStore.currentLevel < AppStore.theLevels.length-1)
-                    return {
+                if(AppStore.currentLevel < AppStore.theLevels.length-1) {
+                    newState = {
                         currentAppLevel:AppStore.theLevels[++AppStore.currentLevel],
                         minLevel:false, maxLevel:false,
                         quiz:AppStore.theQuiz
                     }
 
+                    if(AppStore.localStorageAvailable)
+                        localStorage.setItem(localStorageKey, JSON.stringify(newState))
+
+                    return newState
+                }
                 else
                     return state
 
             case AppActionTypes.LEVEL_RESET:
                 AppStore.currentLevel = 0
                 AppStore.theQuiz = false
-                return {currentAppLevel:AppStore.theLevels[0], minLevel:true, maxLevel:false, quiz:false}
+                newState = AppStore.initialState
+                if(AppStore.localStorageAvailable)
+                    localStorage.setItem(localStorageKey, JSON.stringify(newState))
+                return newState
 
             case AppActionTypes.QUIZ0_TOGGLE:
                 AppStore.theQuiz ^= true
-                return {currentAppLevel:AppStore.theLevels[0], minLevel:true, maxLevel:false, quiz:AppStore.theQuiz}
+                newState = {currentAppLevel:AppStore.theLevels[0], minLevel:true, maxLevel:false, quiz:AppStore.theQuiz}
+                if(AppStore.localStorageAvailable)
+                    localStorage.setItem(localStorageKey, JSON.stringify(newState))
+                return newState
 
             default:
                 return state
@@ -65,5 +102,6 @@ AppStore.theLevels = [
     {app: 4, nounPanel: NounPanelLevel.PLURALIZATION, verbPanel: VerbPanelLevel.PAST_TENSE}
 ]
 AppStore.theQuiz = false
+AppStore.initialState = {currentAppLevel:AppStore.theLevels[0], minLevel:true, maxLevel:false, quiz:false}
 
 export default new AppStore()
